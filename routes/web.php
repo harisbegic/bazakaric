7bca1f597d742f4e93d613c6149fa574586bf25e
harisbegic <haris.begic101@gmail.com>
2020-05-12 23:44:48 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function() {
+	Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+	
+});
